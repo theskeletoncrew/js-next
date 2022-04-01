f1b41bfcf4646d6cb204010102e3be8e95c2b989
Loris Leiva <loris.leiva@gmail.com>
2022-04-01 15:15:16 +0100

    wip

--- tests/drivers/storage/awsStorageDriver.spec.ts
+++ tests/drivers/storage/awsStorageDriver.spec.ts
@@ -2,7 +2,6 @@ import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { awsStorage } from '@/drivers/storage/awsStorageDriver';
 import { MetaplexFile } from '@/drivers/filesystem';
 import { metaplex, teardownMetaplex } from '../../helpers';
-import Sinon from 'sinon';
 import { Metaplex } from '@/Metaplex';
 
 const awsClient = {
@@ -18,13 +17,12 @@ const awsClient = {
 
 describe('AwsStorageDriver', () => {
   let mx: Metaplex;
-
   beforeEach(async () => mx = await metaplex());
   afterEach(() => teardownMetaplex(mx));
 
   it('it can upload assets to a S3 bucket', async () => {
     // Given a mock awsClient.
-    const stub = Sinon.spy(awsClient);
+    const spy = jest.spyOn(awsClient, 'send');
   
     // Fed to a Metaplex instance.
     mx.use(awsStorage(awsClient, 'some-bucket'));
@@ -35,11 +33,14 @@ describe('AwsStorageDriver', () => {
   
     // Then we get the URL of the uploaded asset.
     expect(uri).toEqual('https://s3.us-east.amazonaws.com/some-bucket/some-key');
-    expect(stub.send.calledOnce).toBe(true);
-    const command = stub.send.getCall(0).args[0] as PutObjectCommand;
-    expect(command instanceof PutObjectCommand).toBe(true);
-    expect(command.input.Bucket).toBe('some-bucket');
-    expect(command.input.Key).toBe('some-key');
-    expect(command.input.Body?.toString()).toBe('some-image');
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(expect.any(PutObjectCommand));
+    expect(spy).toHaveBeenCalledWith(expect.objectContaining({
+      input: expect.objectContaining({
+        Bucket: 'some-bucket',
+        Key: 'some-key',
+        Body: expect.any(Buffer),
+      })
+    }));
   });
 });
